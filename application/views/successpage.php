e45548c2df8971ca297ec961a438d31bfc4f65e0
quifar <risqufaris@gmail.com>
2020-08-08 05:45:05 +0700

    Create successpage.php

---
+++ application/views/successpage.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Success</title>
+    <link rel="stylesheet" href="<?php echo base_url('css/style.css');?>">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</head>
+
+<body class="login-body">
+    <div class="container"
+        style="min-height: 100%; min-height: 100vh; display: flex;text-align: center; align-items: center; vertical-align: middle;">
+        <div class="card border-0 shadow my-5 my-auto">
+            <div class="card-body my-5" style="display: inline-block;">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="<?php echo base_url('assets/loginill.png');?>" alt="" class="img-responsive" width="500">
+                        </div>
+                        <div class="col-md-6 border-dark border-left">
+                            <img src="<?php echo base_url('assets/success.jpg');?>" alt="" width=500>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
