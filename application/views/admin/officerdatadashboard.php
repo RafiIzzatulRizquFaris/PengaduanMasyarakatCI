388e92ee632a0de1df5810dbd574f5928f749015
quifar <risqufaris@gmail.com>
2020-08-08 16:50:56 +0700

    Update officerdatadashboard.php

--- application/views/admin/officerdatadashboard.php
+++ application/views/admin/officerdatadashboard.php
@@ -135,15 +135,11 @@
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
-
-                    <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Data Pengaduan</h1>
-                    <p class="mb-4">Mencakup segala data pengaduan yang telah dimasukkan oleh masyarakat</p>
-
+                    
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary text-center">Data Pengaduan</h6>
+                            <h6 class="m-0 font-weight-bold text-primary text-center">Data Petugas</h6>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
