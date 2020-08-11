68c65e6eef28ccae15a3581b4b0b76803621f26d
quifar <risqufaris@gmail.com>
2020-08-11 15:05:06 +0700

    Update OfficerDataController.php
    add fetching data code

--- application/controllers/OfficerDataController.php
+++ application/controllers/OfficerDataController.php
@@ -1,7 +1,15 @@
 <?php
 class OfficerDataController extends CI_Controller{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('ModelAction');
+    }
+
     public function index()
     {
-        $this->load->view('admin/officerdatadashboard');
+        $data['petugas'] = $this->ModelAction->get_masyarakat();
+        $this->load->view('admin/officerdatadashboard', $data);
     }
 }
