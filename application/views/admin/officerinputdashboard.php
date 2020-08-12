bfc39173a2e961ab8c96d236172e2b16d6e04d00
quifar <risqufaris@gmail.com>
2020-08-12 09:30:48 +0700

    Update officerinputdashboard.php
    add logout action

--- application/views/admin/officerinputdashboard.php
+++ application/views/admin/officerinputdashboard.php
@@ -247,7 +247,7 @@
 					<button class="btn btn-secondary" type="button" data-dismiss="modal">
 						Cancel
 					</button>
-					<a class="btn btn-primary" href="login.php">Logout</a>
+					<a class="btn btn-primary" href="<?php echo site_url('Action/logout');?>">Logout</a>
 				</div>
 			</div>
 		</div>
