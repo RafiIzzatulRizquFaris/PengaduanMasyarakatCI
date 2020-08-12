05be976816f414cf78a6533a17522c925b622be5
quifar <risqufaris@gmail.com>
2020-08-12 12:47:17 +0700

    add username in admin field

--- application/views/user/userdashboard.php
+++ application/views/user/userdashboard.php
@@ -118,7 +118,7 @@
 						<li class="nav-item dropdown no-arrow">
 							<a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
 								data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<span class="mr-2 d-none d-lg-inline text-gray-600 small">YourName</span>
+								<span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $this->session->userdata('username') ?></span>
 								<img class="img-profile rounded-circle"
 									src="<?php echo base_url('assets/account_circle.png');?>">
 							</a>
