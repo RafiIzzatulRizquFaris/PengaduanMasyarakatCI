619556f258d54bc9e605b5be0f12028c3290ddaf
quifar <risqufaris@gmail.com>
2020-09-01 08:06:33 +0700

    delete comment

--- application/views/admin/admindashboard.php
+++ application/views/admin/admindashboard.php
@@ -10,8 +10,6 @@
 	<meta name="author" content="">
 
 	<title>Admin Dashboard</title>
-
-	<!-- Custom fonts for this template-->
 	<link
 		href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
 		rel="stylesheet">
